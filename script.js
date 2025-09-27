e597efdab3dd9270fce24d102282fd49044ad0f4
QuasarDev <quasardev9@gmail.com>
2025-09-27 10:23:17 +0000

    Write the logic to play and single round and also debug some errors

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-
+// Logic for the computer to select a choice. Since math.Random genarates numbers from 0-1, we use that to let the computer genenate its choices.
 function getComputerChoice() {
   const random = Math.random();
 
+// Here, if the computer generates a number between 0 and one-third(including one-third) then it means the computer chose Rock.
   if (random > 0 && random <= 1/3) {
     return 'rock';
-  } else if (random > 1/3 && random <= 2/3) {
+  } else if (random > 1/3 && random <= 2/3) { // If it genarates a number between one-third and two-third then it means the computer chose Paper.
     return 'paper';
-  } else if (random > 2/3 && random <= 1) {
+  } else if (random > 2/3 && random <= 1) { // Whereas if, it generates a number between two-third and 1 then the computer chose Scissors.
     return 'scissors';
   }
 }
 
 function getHumanChoice() {
-  return prompt('What\'s your choice:');
+  let choice = prompt('What\'s your choice:').toLowerCase(); // It takes an input form the user.
+  return choice;
 }
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
-
+// These variables will store the game scores.
 let humanScore = 0;
 let computerScore = 0;
 
+
+//Logic for a single round.
+// This place is important so pay attention. We create a function 'playRound' and give it the humanChoice and computerChoice parameters.
+function playRound(humanChoice, computerChoice) {
+  const human = humanChoice.toLowerCase();
+  const computer = computerChoice.toLowerCase();
+
+  if (human === computer) {
+    return "It's a tie";
+  }
+
+  if (
+    (human === "rock" && computer === "scissors") ||
+    (human === "paper" && computer === "rock") ||
+    (human === "scissors" && computer === "paper")
+  ) {
+    humanChoice++
+    return `You win! ${human} beats ${computer}`;
+  } else {
+    computerChoice++
+    return `You lose! ${computer} beats ${human}`;
+  }
+  
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+let result = playRound(humanSelection, computerSelection);
+console.log(result);
